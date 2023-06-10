14ee791177116611ffdec2486cdbe98204df01e1
Dev chaudhary <117721727+devchaudharyy@users.noreply.github.com>
2023-06-11 00:37:59 +0530

    Create Majority element(n%3).cpp

---
+++ Day 4/Majority element(n%3).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int k = nums.size()/3;
+        vector<int> ans;
+        map<int,int> mp;
+        for( auto i: nums){
+            mp[i]++;
+        }
+        for( auto i : mp){
+            if(i.second>k) ans.push_back(i.first);
+        }
+        return ans;
+    }
+};
