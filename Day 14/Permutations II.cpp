a323420b652be4c0b7d16abdeb14a431eaa4d7ec
Dev chaudhary <117721727+devchaudharyy@users.noreply.github.com>
2023-07-02 01:37:35 +0530

    Create Permutations II.cpp

---
+++ Day 14/Permutations II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void func(vector<int> nums, vector<vector<int>> &ans, vector<int> &temp, int freq[])
+    {
+      if(temp.size()==nums.size())
+      {  
+          for(int i = 0;i<ans.size();i++)
+        if(ans[i]==temp) return;
+          ans.push_back(temp); 
+       return ;
+      
+      }
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+             if(freq[i]==0)
+             {
+                 temp.push_back(nums[i]);
+                  freq[i]=1;
+                 func(nums,ans,temp,freq);
+                 freq[i]=0;
+                 temp.pop_back();
+             }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+       vector<vector<int> >ans;
+        vector<int> temp;
+        int n = nums.size();
+        int freq[n];
+        for(int i =0;i<n;i++)
+            freq[i]=0;
+        func(nums,ans,temp,freq);
+        return ans;
+    }
+};
